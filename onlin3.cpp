115732e2b0be9e59b9b9ac5872d0804ace0b4b60
sophat04 <157958030+SormSophat04@users.noreply.github.com>
2024-02-28 21:53:54 +0700

    Add files via upload

---
+++ onlin3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name = "phat";
+    int age = 10;
+    char gender = 'M';
+    double score = 150;
+    bool single = true;
+    string school ="BELTEI_2";
+
+    cout << "My name is:"<< name << " "<<endl;
+    cout << "I am:"<< age <<" year`s old"<<endl;
+    cout << ", and my gender is :"<< gender<<endl ;
+    cout <<" I am a good student within the scosre of :" << score<<endl;
+    cout << " I am single : "<< single << endl;
+    cout << "I am studying at : "<< school << endl;
+
+    return 0;
+}
